fcf67650bf20e1ccf51bb0a16e3540833a457801
anbotkin <59984852+anbotkin@users.noreply.github.com>
2020-02-24 13:54:30 -0500

    Update Player.cpp

--- Catacombs of the Dark Emperor/Code/Player.cpp
+++ Catacombs of the Dark Emperor/Code/Player.cpp
@@ -8,16 +8,17 @@ void Player::Update(Level* level, float elapsed)
 {
 	Vector2f accel = Vector2(0.0f, 0.0f);
 
-	if (Keyboard::isKeyPressed(Keyboard::W))
-	{
-		sprite = sprites[UP];
-		accel.y -= 1.0f;
-	}
 
 	if (Keyboard::isKeyPressed(Keyboard::A))
 	{
 		sprite = sprites[LEFT];
 		accel.x -= 1.0f;
+	}
+
+	if (Keyboard::isKeyPressed(Keyboard::D))
+	{
+		sprite = sprites[RIGHT];
+		accel.x += 1.0f;
 	}
 
 	if (Keyboard::isKeyPressed(Keyboard::S))
@@ -26,11 +27,13 @@ void Player::Update(Level* level, float elapsed)
 		accel.y += 1.0f;
 	}
 
-	if (Keyboard::isKeyPressed(Keyboard::D))
+	if (Keyboard::isKeyPressed(Keyboard::W))
 	{
-		sprite = sprites[RIGHT];
-		accel.x += 1.0f;
+		sprite = sprites[UP];
+		accel.y -= 1.0f;
 	}
+
+	
 
 	// Ensure the movement vector is at most length 1,
 	// to prevent moving faster on diagonals.
