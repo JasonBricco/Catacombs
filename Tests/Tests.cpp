32b4fca291015ae36cd22fff3c685aabf8566d8b
DevStewart19 <devinstew1999@gmail.com>
2020-02-26 12:01:34 -0500

    Added tests for Square and Mul

--- Tests/Tests.cpp
+++ Tests/Tests.cpp
@@ -22,4 +22,38 @@ public:
 		Assert::AreEqual(result.x, 4);
 		Assert::AreEqual(result.y, 3);
 	}
+
+	TEST_METHOD(TestSquare)
+	{
+		float Test = 2;
+		float result = 4;
+		Assert::AreEqual(Square(Test), result);
+		Test = 0;
+		result = 0;
+		Assert::AreEqual(Square(Test), result);
+	}
+
+	TEST_METHOD(TestMul)
+	{
+		Vector2f TestA(1.0f, 1.0f);
+		Vector2f TestB(5.0f, 3.0f);
+		Vector2f result1 = Mul(TestA, TestB);
+
+		Assert::AreEqual(result1.x, 5.0f);
+		Assert::AreEqual(result1.y, 3.0f);
+
+		Vector2f TestC(0.0f, 2.0f);
+		Vector2f TestD(100.0f, 5.0f);
+		Vector2f result2 = Mul(TestC, TestD);
+
+		Assert::AreEqual(result2.x, 0.0f);
+		Assert::AreEqual(result2.y, 10.0f);
+
+		Vector2f TestE(0.5f, 1.5f);
+		Vector2f TestF(5.0f, 3.0f);
+		Vector2f result3 = Mul(TestE, TestF);
+
+		Assert::AreEqual(result3.x, 2.5f);
+		Assert::AreEqual(result3.y, 4.5f);
+	}
 };
